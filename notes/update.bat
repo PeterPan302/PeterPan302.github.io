70e8987be232a2ef51ee6662183be512556e21cb
Peter <3106256881@qq.com>
2022-09-29 22:20:07 +0800

    a

--- notes/update.bat
+++ notes/update.bat
@@ -1,10 +1,11 @@
 @echo off
-echo ^<!DOCTYPE html^>                 > index.html
-echo ^<html^>                         >> index.html
-echo 	^<head^>                      >> index.html
-echo 		^<title^>Index^</title^>  >> index.html
-echo 	^</head^>                     >> index.html
-echo 	^<body^>                      >> index.html
+echo ^<!DOCTYPE html^>                  >  index.html
+echo ^<html^>                           >> index.html
+echo 	^<head^>                        >> index.html
+echo 		^<meta charset="utf-8" /^>  >> index.html
+echo 		^<title^>Index^</title^>    >> index.html
+echo 	^</head^>                       >> index.html
+echo 	^<body^>                        >> index.html
 for /r . %%i in (*.html) do (echo 		^<a href="%%~nxi"^>%%~nxi^</a^>^<br /^> >> index.html)
-echo 	^</body^>                     >> index.html
-echo ^</html^>                        >> index.html
+echo 	^</body^>                       >> index.html
+echo ^</html^>                          >> index.html
